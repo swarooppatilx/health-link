e7449035d3b15652bb906255b4f0ca9f85de3cd3
swarooppatilx <swarooppatil850@gmail.com>
2024-09-03 00:43:35 +0530

    prettier

--- src/components/home/health_section.tsx
+++ src/components/home/health_section.tsx
@@ -32,27 +32,27 @@ const HealthSection = () => {
   }
 
   return (
-    <div className="mb-6 p-2">
-      <div className="mb-2 flex justify-between">
-        <h3 className="font-bold">Health</h3>
-        <Link href="/health">
-          <p className="text-blue-600">View all</p>
+    <div className='mb-6 p-2'>
+      <div className='mb-2 flex justify-between'>
+        <h3 className='font-bold'>Health</h3>
+        <Link href='/health'>
+          <p className='text-blue-600'>View all</p>
         </Link>
       </div>
-      <div className="rounded-lg bg-white shadow-md">
+      <div className='rounded-lg bg-white shadow-md'>
         {data.slice(0, 3).map((item, index) => (
           <div key={index}>
             {item.hasLink ? (
               <Link href={item.link ?? '#'}>
-                <div className="flex cursor-pointer items-center justify-between border-b p-4 font-bold text-nhs-blue">
+                <div className='flex cursor-pointer items-center justify-between border-b p-4 font-bold text-nhs-blue'>
                   <span>{item.title}</span>
-                  <FontAwesomeIcon icon={faChevronRight} className="h-6 w-6" />
+                  <FontAwesomeIcon icon={faChevronRight} className='h-6 w-6' />
                 </div>
               </Link>
             ) : (
-              <div className="flex items-center justify-between border-b p-4 font-bold text-nhs-blue">
+              <div className='flex items-center justify-between border-b p-4 font-bold text-nhs-blue'>
                 <span>{item.title}</span>
-                <FontAwesomeIcon icon={faChevronRight} className="h-6 w-6" />
+                <FontAwesomeIcon icon={faChevronRight} className='h-6 w-6' />
               </div>
             )}
           </div>
--- src/components/messages/messages.tsx
+++ src/components/messages/messages.tsx
@@ -25,7 +25,6 @@ const MessagesComponent = () => {
     void fetchData();
   }, []);
 
-
   if (loading) {
     return <Spinner />;
   }
