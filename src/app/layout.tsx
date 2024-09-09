b803dca54e3206ad4eb6b303f38055f2059053c7
swarooppatilx <swarooppatil850@gmail.com>
2024-09-09 21:48:43 +0530

    CHORE: fix layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,17 @@ import '@/styles/globals.css';
 
 import { GeistSans } from 'geist/font/sans';
 import { type Metadata } from 'next';
-
 import { TRPCReactProvider } from '@/trpc/react';
 
 export const metadata: Metadata = {
   title: 'Health Link',
   description:
-    'Your one stop solution for hospital appointments, health records, and more',
+    'Your one-stop solution for hospital appointments, health records, and more.',
   manifest: '/manifest.json',
-  icons: [{ rel: 'icon', url: '/favicon.ico' }],
+  icons: [
+    { rel: 'icon', url: '/favicon.ico' },
+    { rel: 'apple-touch-icon', url: '/icon-.png', sizes: '180x180' },
+  ],
   authors: [{ name: 'Swaroop Patil', url: 'https://swarooppatilx.github.io' }],
   keywords:
     'health, hospital, appointments, health records, medical, health management',
@@ -18,9 +20,16 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'Health Link',
     description:
-      'Your one stop solution for hospital appointments, health records, and more',
+      'Your one-stop solution for hospital appointments, health records, and more.',
     url: 'https://health-link-v1.vercel.app',
-    images: 'https://health-link-v1.vercel.app/og-image.jpg',
+    images: [
+      {
+        url: 'https://health-link-v1.vercel.app/og-image.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Health Link - One Stop Solution for Health Management',
+      },
+    ],
     type: 'website',
   },
   twitter: {
@@ -28,8 +37,19 @@ export const metadata: Metadata = {
     site: '@sasta.tony.stark',
     title: 'Health Link',
     description:
-      'Your one stop solution for hospital appointments, health records, and more',
-    images: 'https://health-link-v1.vercel.app/twitter-image.jpg',
+      'Your one-stop solution for hospital appointments, health records, and more.',
+    images: [
+      {
+        url: 'https://health-link-v1.vercel.app/twitter-image.jpg',
+        alt: 'Health Link Twitter Image',
+      },
+    ],
+  },
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: false,
   },
 };
 
@@ -37,9 +57,11 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang='en' className={`${GeistSans.variable}`}>
+    <html lang="en" className={`${GeistSans.variable}`}>
       <body>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+      <div className="max-w-md md:m-auto">
+          <TRPCReactProvider>{children}</TRPCReactProvider>
+        </div>
       </body>
     </html>
   );
